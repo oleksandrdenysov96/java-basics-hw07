dd410f188777507e824814cdc0faeadbd65e4e07
Oleksandr Denysov <denisovalexsd@gmail.com>
2020-04-27 00:49:01 +0300

    Done HW7

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -11,8 +11,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int lowerBound, int upperBound) {
-    // TODO fill in code here using for loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int i = lowerBound; i <= upperBound; i++) {
+      sum += i;
+    }
+    return sum;
   }
 
   /**
@@ -22,8 +25,11 @@ public class SumAverage {
    * @return sum of integers
    */
   public static int sum(int[] numbers) {
-    // TODO fill in code here using for each loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int number : numbers) {
+      sum += number;
+    }
+    return sum;
   }
 
   /**
@@ -34,8 +40,13 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int lowerBound, int upperBound) {
-    // TODO fill in code here using while loop and replace the return statement
-    return -1;
+    int sum = 0;
+    int count = 0;
+    for (int i = lowerBound; i <= upperBound; i++) {
+      sum += i;
+      count++;
+    }
+    return (double) sum / count;
   }
 
   /**
@@ -45,7 +56,12 @@ public class SumAverage {
    * @return average of integers
    */
   public static double average(int[] numbers) {
-    // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
+    double sum = 0;
+    double count = 0;
+    for (int number : numbers) {
+      sum += number;
+      count++;
+    }
+    return sum / count;
   }
 }
